355f35c2352c36428e7a1f8fb10f58ce43be3cb3
igorspinola <igorspinoladev@gmail.com>
2026-05-23 15:19:07 -0300

    run initial script

---
+++ sql_output/01_tb_status_pagamento.sql
@@ -0,0 +1,5 @@
+-- Tabela de referencia: status de pagamento
+INSERT INTO tb_status_pagamento (id_pagamento, no_status_pagamento) VALUES
+  (1, 'CONCLUIDO'),
+  (2, 'PENDENTE'),
+  (3, 'CANCELADO');
---
+++ sql_output/02_tb_tipo_publicacao.sql
@@ -0,0 +1,4 @@
+-- Tabela de referencia: tipos de publicacao
+INSERT INTO tb_tipo_publicacao (id_tipo_publicacao, no_tipo_publicacao) VALUES
+  (1, 'Artigo'),
+  (2, 'Resumo');
