197f56699c3375d21c109a013035ad965e5a802d
igorspinola <igorspinoladev@gmail.com>
2026-05-24 18:30:59 -0300

    add query 13

--- sql_avancado/q13.sql
+++ sql_avancado/q13.sql
@@ -3,3 +3,11 @@
 -- Tabelas: participante, inscricao, evento
 -- Funções: SUB-CONSULTAS + JOIN
 -- Requisito: identificar público de alto valor
+
+  SELECT DISTINCT p.no_participante, p.ds_email
+  FROM rl_inscricao AS i
+  JOIN tb_participante AS p ON i.id_participante = p.id_participante
+  WHERE i.id_evento IN (
+      SELECT id_evento FROM tb_evento
+      WHERE vl_preco = (SELECT MAX(vl_preco) FROM tb_evento)
+  );
