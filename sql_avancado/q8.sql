b8862abcbe67f7b7b115491039b9b05f8d70e4b0
deiv <deivid652015@gmail.com>
2026-05-24 13:44:48 -0300

    adiciona consulta para listar participantes com 2+ inscrições no mesmo mês

--- sql_avancado/q8.sql
+++ sql_avancado/q8.sql
@@ -3,3 +3,36 @@
 -- Tabelas: participante, inscricao, evento
 -- Funções: JOIN + GROUP BY + COUNT (com HAVING)
 -- Requisito: identificar comportamento de consumo intenso
+
+select
+    tb_participante.id_participante,
+    tb_participante.no_participante,
+    extract(
+        month
+        from rl_inscricao.dh_pagamento
+    ) as mes,
+    extract(
+        year
+        from rl_inscricao.dh_pagamento
+    ) as ano,
+    count(*) as qt_inscricoes
+from
+    tb_participante
+    join rl_inscricao on rl_inscricao.id_participante = tb_participante.id_participante
+    join tb_evento on tb_evento.id_evento = rl_inscricao.id_evento
+where
+    rl_inscricao.dh_pagamento is not null
+group by
+    tb_participante.id_participante,
+    tb_participante.no_participante,
+    extract(
+        month
+        from rl_inscricao.dh_pagamento
+    ),
+    extract(
+        year
+        from rl_inscricao.dh_pagamento
+    )
+having
+    count(*) > 1
+order by qt_inscricoes desc, no_participante;
