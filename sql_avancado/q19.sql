8765103a69fe64e817a6feeaacf8fd04e0aa353f
dougg <douglas.aleixo2806@gmail.com>
2026-05-24 21:58:45 -0300

    feat: add query 19 e 20

--- sql_avancado/q19.sql
+++ sql_avancado/q19.sql
@@ -1,5 +1,14 @@
 -- Query 19: Eventos onde a maioria pagou via PIX
--- Objetivo: Listar eventos onde mais de 50% das inscrições CONCLUIDO foram via PIX.
+-- Objetivo: Listar eventos onde mais de 40% das inscrições CONCLUIDO foram via PIX.
 -- Tabelas: evento, inscricao, status_pagamento
 -- Funções: SUB-CONSULTAS + JOIN + GROUP BY + COUNT + HAVING
 -- Requisito: análise de preferência de pagamento
+
+select id_evento from(
+  select id_evento, count(case when ds_metodo_pagamento = 'pix' then 1 end) as total_pix,
+    count(nu_inscricao) as total_pago
+    from rl_inscricao
+    where cd_status_pagamento = 1
+    group by id_evento)
+where total_pix > total_pago * 0.4
+order by id_evento asc
--- sql_avancado/q20.sql
+++ sql_avancado/q20.sql
@@ -3,3 +3,16 @@
 -- Tabelas: participante, inscricao, evento, status_pagamento, autoria, publicacao
 -- Funções: SUB-CONSULTAS + JOIN + GROUP BY + WINDOW (RANK) + COUNT
 -- Requisito: dashboard individual de participante
+
+select tb_participante.id_participante, count(rl_inscricao.id_evento) as tot_eventos,
+  count(case when st_aprovado = true then 1 end) as tot_publicacoes_aprovadas,
+  sum(vl_preco) as tot_gastos_em_eventos,
+  rank() over(
+    order by sum(vl_preco) desc
+  ) as rank_maior_gasto
+  from tb_participante
+  left join rl_inscricao on rl_inscricao.id_participante = tb_participante.id_participante
+  left join tb_evento on tb_evento.id_evento = rl_inscricao.id_evento
+  left join rl_autoria on rl_autoria.id_participante = tb_participante.id_participante
+  left join tb_publicacao on tb_publicacao.id_publicacao = rl_autoria.id_publicacao
+  group by tb_participante.id_participante
